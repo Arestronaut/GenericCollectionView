f4b6049b04ef2e98d8d11597f98b8c8832b45947
Raoul Schwagmeier <r.schwagemeier@gmail.com>
2019-04-22 10:28:58 +0200

    Add Carthage support

---
+++ Carthage/Build/iOS/GenericCollectionView.framework/Headers/GenericCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  GenericCollectionView.h
+//  GenericCollectionView
+//
+//  Created by Raoul Schwagmeier on 19.04.19.
+//  Copyright © 2019 raoulschwagmeier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericCollectionView.
+FOUNDATION_EXPORT double GenericCollectionViewVersionNumber;
+
+//! Project version string for GenericCollectionView.
+FOUNDATION_EXPORT const unsigned char GenericCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericCollectionView/PublicHeader.h>
+
+
